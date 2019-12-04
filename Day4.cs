3c2a756c01f3c873c638a4ef4c7552bcc969aa66
CodingNagger <contact@iamnguele.com>
2019-12-04 07:42:10 +0000

    Day 4 completed

---
+++ Day4.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Linq;
+
+namespace adventofcode_2019
+{
+    class Day4 : Day
+    {
+        public override string Compute(string[] input)
+        {
+            int[] range = input[0].Split('-').Select(r => int.Parse(r)).ToArray();
+            int validPasswordCounts = 0;
+
+            for (int cursor = range[0]; cursor <= range[1]; cursor++)
+            {
+                if (isValidPassword($"{cursor}"))
+                {
+                    validPasswordCounts++;
+                }
+            }
+
+            return $"{validPasswordCounts}";
+        }
+
+        bool isValidPassword(string password)
+        {
+            int cursor = 0;
+            bool validDoublePairsFound = false;
+            int currentMatchingStreak = 0;
+            bool neverDecreases = true;
+
+            while (cursor < password.Length - 1)
+            {
+                currentMatchingStreak = 0;
+                while (cursor < password.Length - 1 && password[cursor] == password[cursor + 1])
+                {
+                    cursor++;
+                    currentMatchingStreak++;
+                }
+
+                if (cursor < password.Length - 1)
+                {
+                    if (password[cursor] > password[cursor + 1])
+                    {
+                        neverDecreases = false;
+                    }
+
+                    if (currentMatchingStreak == 1)
+                    {
+                        validDoublePairsFound = true;
+                    }
+                    else
+                    {
+                        cursor++;
+                    }
+                }
+            }
+
+            if (currentMatchingStreak == 1)
+            {
+                validDoublePairsFound = true;
+            }
+
+            return cursor == password.Length - 1 && validDoublePairsFound && neverDecreases;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,10 +11,9 @@ namespace adventofcode_2019
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
 
-            Day day = new Day3();
-            // var input = new string[] { "R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83" };
-            // var input = new string[] { "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7" };
-            var input = File.ReadAllLines("./input/day3.txt");
+            Day day = new Day4();
+            // var input = new string[] { "123455-123456" };
+            var input = File.ReadAllLines("./input/day4.txt");
             Console.WriteLine(day.Compute(input));
 
             stopWatch.Stop();
