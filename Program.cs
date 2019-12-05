cdb0630dc30d2252474dbc20b963f29fac267d89
CodingNagger <contact@iamnguele.com>
2019-12-05 15:05:38 +0000

    About time to push that lunchtime fix, day 5 is probably the last one I'm doing before gym and coffee.

--- Program.cs
+++ Program.cs
@@ -11,9 +11,9 @@ namespace adventofcode_2019
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
 
-            Day day = new Day4();
-            // var input = new string[] { "123455-123456" };
-            var input = File.ReadAllLines("./input/day4.txt");
+            Day day = new Day5();
+            // var input = new string[] { "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99" };
+            var input = File.ReadAllLines("./input/day5.txt");
             Console.WriteLine(day.Compute(input));
 
             stopWatch.Stop();
