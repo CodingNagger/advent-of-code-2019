b5ab0de12ee62082cdbce918934d4bbccb5c69e1
Jean-Dominique Nguélé <contact@iamnguele.com>
2019-12-09 05:13:47 +0000

    Renaming some stuff properly

--- adventofcode-2019/Reusability/IntCodeComputer.cs
+++ adventofcode-2019/Reusability/IntCodeComputer.cs
@@ -5,10 +5,24 @@ namespace AdventOfCode2019
 {
     public class IntCodeComputer
     {
-        public IntCodeComputer(int phaseSetting, long[] baseValues)
+        Queue<long> inputs;
+
+        long? latestOutput;
+        long[] program;
+        long cursor;
+
+        public long Noun { set { program[1] = value; } }
+        public long Verb { set { program[2] = value; } }
+        public bool ShouldContinue => (OperationType) program[cursor] != OperationType.Halt;
+
+        public long FirstValue => program[0];
+
+        public long? LatestOutput => latestOutput;
+
+        public IntCodeComputer(int phaseSetting, long[] program)
         {
-            this.values = new long[baseValues.Length];
-            baseValues.CopyTo(this.values, 0);
+            this.program = new long[program.Length];
+            program.CopyTo(this.program, 0);
             cursor = 0;
             latestOutput = null;
             inputs = new Queue<long>();
@@ -19,44 +33,30 @@ namespace AdventOfCode2019
         {
         }
 
-        Queue<long> inputs;
-
-        long? latestOutput;
-        long[] values;
-        long cursor;
-
-        public long Noun { set { values[1] = value; } }
-        public long Verb { set { values[2] = value; } }
-        public bool ShouldContinue => (OperationType) values[cursor] != OperationType.Halt;
-
-        public long FirstValue => values[0];
-
-        public long? LatestOutput => latestOutput;
-
         public long? RunIntcodeProgram(long nextInput = 0)
         {
             this.inputs.Enqueue(nextInput);
 
             while (ShouldContinue)
             {
-                var operationType = (OperationType) (values[cursor] % 100);
-                var param1Mode = (ValueMode) ((values[cursor] / 100) % 10);
-                var param2Mode = (ValueMode) ((values[cursor] / 1000) % 10);
-                var param3Mode = (ValueMode) ((values[cursor] / 10000) % 10);
+                var operationType = (OperationType) (program[cursor] % 100);
+                var param1Mode = (ValueMode) ((program[cursor] / 100) % 10);
+                var param2Mode = (ValueMode) ((program[cursor] / 1000) % 10);
+                var param3Mode = (ValueMode) ((program[cursor] / 10000) % 10);
 
                 if (operationType == OperationType.Add)
                 {
-                    values[ValueFor(3, ValueMode.Immediate)] = ValueFor(1, param1Mode) + ValueFor(2, param2Mode);
+                    program[ValueFor(3, ValueMode.Immediate)] = ValueFor(1, param1Mode) + ValueFor(2, param2Mode);
                     cursor += 4;
                 }
                 else if (operationType ==  OperationType.Multiply)
                 {
-                    values[ValueFor(3, ValueMode.Immediate)] = ValueFor(1, param1Mode) * ValueFor(2, param2Mode);
+                    program[ValueFor(3, ValueMode.Immediate)] = ValueFor(1, param1Mode) * ValueFor(2, param2Mode);
                     cursor += 4;
                 }
                 else if (operationType == OperationType.Store)
                 {
-                    values[ValueFor(1, ValueMode.Immediate)] =  GetDirtyInput();
+                    program[ValueFor(1, ValueMode.Immediate)] =  GetDirtyInput();
                     cursor += 2;
                 }
                 else if (operationType == OperationType.Output)
@@ -75,12 +75,12 @@ namespace AdventOfCode2019
                 }
                 else if (operationType == OperationType.Less)
                 {
-                    values[ValueFor(3, ValueMode.Immediate)] = ValueFor(1, param1Mode) < ValueFor(2, param2Mode) ? 1 : 0;
+                    program[ValueFor(3, ValueMode.Immediate)] = ValueFor(1, param1Mode) < ValueFor(2, param2Mode) ? 1 : 0;
                     cursor += 4;
                 }
                 else if (operationType == OperationType.Equals)
                 {
-                    values[ValueFor(3, ValueMode.Immediate)] = ValueFor(1, param1Mode) == ValueFor(2, param2Mode) ? 1 : 0;
+                    program[ValueFor(3, ValueMode.Immediate)] = ValueFor(1, param1Mode) == ValueFor(2, param2Mode) ? 1 : 0;
                     cursor += 4;
                 }
             }
@@ -97,11 +97,11 @@ namespace AdventOfCode2019
         {
             if (mode == ValueMode.Position)
             {
-                return values[values[cursor + relativePos]];
+                return program[program[cursor + relativePos]];
             }
             else if (mode == ValueMode.Immediate)
             {
-                return values[cursor + relativePos];
+                return program[cursor + relativePos];
             }
 
             throw new ArgumentException($"Why you do this? Mode: {mode}");
